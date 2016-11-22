d4717397eceb3887b1664de2ad627de319c0f94d
Josh 'VaMoose' harington <josh1@live.co.za>
2016-11-22 09:14:34 +0200

    Added some comments to Helper.php

--- Helper.php
+++ Helper.php
@@ -5,9 +5,19 @@
  * User: Josh Harington
  * Date: 11/22/2016
  * Time: 9:00 AM
+ *
+ * Description: A class with a few methods to help you with some operations.
  */
 class Helper {
 
+    /**
+     * Create a slug from a string
+     *
+     * eg: This Web Page -> this-web-page
+     *
+     * @param $string
+     * @return string
+     */
     static function string_to_slug($string) {
         $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
         $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
